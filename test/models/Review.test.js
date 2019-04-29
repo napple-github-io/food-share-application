e98c927400826e518562b031be2cd5217248fd4a
Cara Ponzini <ponzini@gmail.com>
2019-04-29 16:01:22 -0700

    Review model tests passing

--- test/models/Review.test.js
+++ test/models/Review.test.js
@@ -4,24 +4,50 @@ const Review = require('../../lib/models/Review');
 
 
 describe('Review Model', () => {
-  const id = new mongoose.Types.ObjectId;
-
-  it('validates a good Review model', () => {
-    const review = new Review({
-      reviewer: id,
-      reviewee: id,
-      reviewText: 'super punctual',
-      good: true,
-    });
-   
-    expect(review).toEqual({
-      reviewer: id,
-      reviewee: id,
-      reviewText: 'super punctual',
-      good: true,
-      timestamp: expect.any(String),
-      _id: expect.any(mongoose.Types.ObjectId),
-      __v: 0
+  beforeAll(() => {
+    return mongoose.connect('mongodb://localhost:27017/napple', {
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useNewUrlParser: true
     });
   });
+    
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+    
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+      
+  const id = new mongoose.Types.ObjectId;
+
+  const review = new Review({
+    reviewer: id,
+    reviewee: id,
+    reviewText: 'super punctual',
+    good: true,
+  });
+  it('validates a good Review model', () => {
+
+    expect(review.toJSON()).toEqual({
+      reviewer: id,
+      reviewee: id,
+      reviewText: 'super punctual',
+      good: true,
+      _id: expect.any(mongoose.Types.ObjectId)
+    });
+  });
+
+  it('has some required fields', () => {
+    const malformedReview = new Review({
+      reviewText: 'super punctual',
+    });
+
+    const errors = malformedReview.validateSync().errors;
+    expect(errors.reviewer.message).toEqual('Path `reviewer` is required.');
+    expect(errors.reviewee.message).toEqual('Path `reviewee` is required.');
+    expect(errors.good.message).toEqual('Path `good` is required.');
+  });
+
 });
