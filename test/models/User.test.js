fd279fec4423f90db453a8ddee8b0b8d0a443aef
Sean Nelson <smnelsonn@gmail.com>
2019-04-29 11:48:33 -0700

    User compare passed

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,9 +1,25 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const User = require('../../lib/models/User');
-const { hash } = require('../../lib/utils/hash');
 
 describe('User model', () =>{
+
+  beforeAll(() => {
+    return mongoose.connect('mongodb://localhost:27017/napple', {
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useNewUrlParser: true
+    });
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
   const user = new User({
     username: 'wookie',
     password: 'goobers',
@@ -22,5 +38,20 @@ describe('User model', () =>{
 
   it('returns with a passwordHash', () => {
     expect(user._tempPassword).toEqual('goobers');
+  });
+
+  it('compares a good password', () => {
+    return User.create({
+      username: 'wookie',
+      password: 'goobers',
+      email: 'feet@shoes.com',
+      address: '1919 NW Quimby St., Portland, Or 97209'
+    })
+      .then(created => {
+        return created.compare('goobers');
+      })
+      .then(result => {
+        expect(result).toBeTruthy();
+      });
   });
 });
