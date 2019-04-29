536728b2d2f9ee57b1df0a42470be45bbad4559b
Sean Nelson <smnelsonn@gmail.com>
2019-04-29 13:34:26 -0700

    ensure auth test passing

---
+++ test/middleware/ensureAuth.test.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const { tokenize } = require('../../lib/utils/token');
+const { ensureAuth } = require('../../lib/middleware/ensureAuth');
+
+describe('ensureAuth middleware', () => {
+  it('validates a valid token', done => {
+    const token = tokenize({
+      username: 'banana'
+    });
+
+    const req = { token };
+    const res = {};
+    const next = () => {
+      expect(req.user).toEqual({ username: 'banana' });
+      done();
+    };
+
+    ensureAuth(req, res, next);
+  });
+});
