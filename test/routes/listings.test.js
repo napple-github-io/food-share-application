77f2be9424c8fa47cb2b34693e935adaa4bad40e
Sean Nelson <smnelsonn@gmail.com>
2019-05-02 16:17:08 -0700

    dietary searches, both normal and with zip/radius

--- test/routes/listings.test.js
+++ test/routes/listings.test.js
@@ -405,7 +405,7 @@ describe('listings routes', () => {
       });
   });
 
-  it.only('searches by dietary restriction', () => {
+  it('searches by dietary restriction', () => {
     return request(app)
       .post('/api/v1/auth/signup')
       .send(user)
@@ -423,6 +423,31 @@ describe('listings routes', () => {
           .then(() => {
             return request(app)
               .get('/api/v1/listings/dietary?dairy=true&gluten=true')
+              .then(res => {
+                expect(res.body[0].dietary.gluten).toEqual(true);
+              });
+          });   
+      });
+  });
+  
+  it('searches by dietary restriction in a specific zip', () => {
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send(user)
+      .then(createdUser => {
+        return request(app)
+          .post('/api/v1/listings')
+          .send({
+            title: 'carrots and beans',
+            user: createdUser.body.user._id,
+            location: { address: '1919 NW Quimby St., Portland, Or', zip: '97201' },
+            category: 'produce',
+            dietary: { dairy: true, gluten: true, nut: false }
+          })
+          .set('Authorization', `Bearer ${createdUser.body.token}`)
+          .then(() => {
+            return request(app)
+              .get('/api/v1/listings/dietary/close?dairy=true&gluten=true&zip=97201&radiusInMiles=5')
               .then(res => {
                 expect(res.body[0].dietary.gluten).toEqual(true);
               });
